34f6b99d4780c1fd343bb45b003e72c2c49d6dc7
Troy <troyingram86@gmail.com>
2021-05-04 12:51:43 +0000

    initial commit

---
+++ brandnamegenerator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Program Greeting
+print("Welcome to the Brand Name Generator!")
+
+# Ask user what City they grew up in and set to variable
+city = input("What City did you grow up in?: ")
+
+# Ask user for their pet's name and set to variable
+pet = input("What's your pet's name?: ")
+
+# Print Brand Name combining city and pet variables
+print("Your Brand Name could be " + city + " " + pet)
