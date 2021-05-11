5f73d2163e4d6da4b15a77905ed62af51d0f0ae1
EC2 Default User <ec2-user@ip-172-31-19-254.ec2.internal>
2021-05-11 13:44:49 +0000

    added comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,14 @@
+#Print numbers from 1-100
 for i in range(1,101):
+#if multiple of 3 and 5 print "FizzBuzz"    
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
+#If multiple of 3 print "Fizz"
     elif i % 3 == 0:
         print("Fizz")
+#If multiple of 5 pring "Buzz"
     elif i % 5 == 0:
         print("Buzz")
+#Print all other integers
     else:
         print(i)
