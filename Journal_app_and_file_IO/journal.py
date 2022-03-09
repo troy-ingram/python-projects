22a7186353d18b0ee27f6e325f944bf394e8a442
Troy <ubuntu@ip-172-31-68-164.ec2.internal>
2022-03-09 20:58:13 +0000

    added absolute filename

--- Journal_app_and_file_IO/journal.py
+++ Journal_app_and_file_IO/journal.py
@@ -1,10 +1,31 @@
+import os
 
 def load(name):
     #todo: populate from file if it exists.
-    return []
+    data = []
+    filename = get_full_pathname(name)
+    
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
 
 def save(name, journal_data):
-    pass
+    filename = get_full_pathname(name)
+    print("..... saving to: {}".format(filename))
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('./python-projects/Journal_app_and_file_IO/journals', name + '.jrl'))
+    return filename
+
 
 def add_entry(text, journal_data):
     journal_data.append(text)
