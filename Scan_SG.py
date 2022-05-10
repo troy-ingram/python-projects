411b3aa6f4a9b74689c4241b0c0c00390c9ec028
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-05-10 10:53:15 +0000

    updated comments

--- Scan_SG.py
+++ Scan_SG.py
@@ -2,32 +2,34 @@ import boto3
 
 ec2 = boto3.client('ec2')
 sg_rules = ec2.describe_security_group_rules()
+security_groups_rules = sg_rules['SecurityGroupRules']
 
 ec2_resource = boto3.resource('ec2')
 
 
-cidr_violators = []
+#blank dictionary to add violators 
 sg_dict = {}
 
-security_groups_rules = sg_rules['SecurityGroupRules']
-# print(security_groups_rules)
+#iterate through security group rules
 for security_group_rule in security_groups_rules:
     port_number = security_group_rule.get('FromPort', 'No Port Listed')
     cidrs = security_group_rule.get('CidrIpv4', 'No Cidr Listed')
+    #if open to the world and not HTTP or HTTPS then update sg_dict with Rule ID and Group Id
     if cidrs == '0.0.0.0/0' and port_number != 80 and port_number != 443 and not security_group_rule['IsEgress']:
-        print(security_group_rule['GroupId'] + 'is in violation and rule will be deleted for port ' + str(port_number) + ' : ' + cidrs)
-        cidr_violators.append(security_group_rule['SecurityGroupRuleId'])
+        print(security_group_rule['GroupId'] + ' is in violation and rule will be deleted for port ' + str(port_number) + ' : ' + cidrs)
         sg_dict.update({ security_group_rule['SecurityGroupRuleId'] : security_group_rule['GroupId'] })
 
+#if no entries in sg_dict then there are no rules in violation
 if len(sg_dict) == 0:
     print('No Security Group Violations')
 
-for x, y in sg_dict.items():
-    security_group = ec2_resource.SecurityGroup(y)
+#iterate through sg_dict and for each entry revoke ingress using the Security Group ID and the Security Group Rule ID
+for RuleID, SGID in sg_dict.items():
+    security_group = ec2_resource.SecurityGroup(SGID)
     security_group.revoke_ingress(
         SecurityGroupRuleIds=[
-            x,
-            ]
+            RuleID,
+        ]
     )
 
 
